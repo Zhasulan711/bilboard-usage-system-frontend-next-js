7fef871fb200a01d9bff46e246e0171c5a0826c7
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-02-15 01:29:04 +0600

    set up next and creating initial structure architecture code

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+export default function Home() {
+  return (
+    <main className="">
+    </main>
+  );
+}
