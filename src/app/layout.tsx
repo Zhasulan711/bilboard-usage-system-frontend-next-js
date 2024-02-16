2c37b41733a5b1837b16e70be91f7d4d858de3e8
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-02-16 12:34:04 +0600

    adding responsive design to html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="hidden mcb-pro-16:block">
       <body>{children}</body>
     </html>
   );
