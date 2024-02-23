d2e9c840be6cbce0035859f6758fa87ea6713995
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-02-24 03:58:32 +0600

    markup style sign-up page

--- src/components/MenuSidebar/MenuSidebar.tsx
+++ src/components/MenuSidebar/MenuSidebar.tsx
@@ -4,10 +4,12 @@ import Link from "next/link";
 import { NAVBAR_LIST } from "@/constants";
 import { Icon } from "../Icons/Icon";
 import { PlusIcon } from "../Icons";
+import { ContactWithUsIcon } from "../Icons";
+import { LogOutIcon } from "../Icons";
 
 export default function MenuSidebar() {
   return (
-    <aside className="w-[342px] bg-[#0F1623] h-[100vh] pt-[88px] pl-[30px]">
+    <aside className="w-[342px] bg-[#0F1623] h-[100vh] pt-[88px] px-[30px]">
       <nav className="flex flex-col">
         {NAVBAR_LIST.map(({ title, icon }, index) => {
           const href = "/" + title.toLowerCase();
@@ -28,6 +30,36 @@ export default function MenuSidebar() {
             </div>
           );
         })}
+
+        {/* Contact With US and LogOut */}
+        <div className="border-t-2 border-[#182235] mt-[18px]"></div>
+        <div
+          className="flex flex-row gap-[10px] items-center pl-2 w-[286px] h-[46px] rounded-lg
+              hover:bg-[#182235] focus:bg-[#182235] transition duration-300 ease-in-out mt-[18px]"
+        >
+          <ContactWithUsIcon />
+          <Link
+            href="#"
+            className="text-[#72B152] text-[19px] font-normal 
+                hover:text-[#F9B13C] transition duration-300 ease-in-out"
+          >
+            Contact With Us
+          </Link>
+        </div>
+
+        <div
+          className="flex flex-row gap-[10px] items-center pl-2 w-[286px] h-[46px] rounded-lg
+              hover:bg-[#182235] focus:bg-[#182235] transition duration-300 ease-in-out"
+        >
+          <LogOutIcon />
+          <Link
+            href="/"
+            className="text-[#D6343D] text-[19px] font-normal 
+                hover:text-[#F9B13C] transition duration-300 ease-in-out"
+          >
+            Log Out
+          </Link>
+        </div>
 
         {/* post new ads */}
         <Link
