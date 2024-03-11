a9f6073cb92c2c77e56dc1442b3720ad31fa6ea6
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-03-11 17:49:36 +0500

    adding HeaderSearchBar logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,41 +1,15 @@
 "use client";
 
 import Image from "next/image";
-import { useState, useRef, ChangeEvent } from "react";
-import { BellIcon, SearchIcon } from "../Icons";
+import { BellIcon } from "../Icons";
+import HeaderSearchBar from "../HeaderSearchBar/HeaderSearchBar";
 
 export default function Header() {
-  const [searchText, setSearchText] = useState("");
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const handleSVGClick = (current: HTMLInputElement | null) => {
-    if (current) {
-      current.focus();
-    }
-  };
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchText(e.target.value);
-  };
-
   return (
     <header className="w-[1386px] bg-[#0F1623] h-[68px] ml-auto flex border-l-2 border-[#010714]">
       <div className="flex flex-row pl-[20px] space-x-[820px] items-center">
         {/* search bar */}
-        <div
-          className="w-[321px] h-[42px] bg-[#182235] rounded-lg pl-[10px]
-        flex flex-row space-x-[6px] items-center "
-        >
-          <SearchIcon handleSVGClick={() => handleSVGClick(inputRef.current)} />
-          <input
-            ref={inputRef}
-            className="placeholder-[#6E7581] text-sm font-normal bg-[#182235] text-[#6E7581] focus:outline-none"
-            type="search"
-            placeholder="Search"
-            value={searchText}
-            onChange={handleChange}
-          />
-        </div>
+        <HeaderSearchBar />
 
         {/* notification and user information */}
         <div className="flex flex-row space-x-[15px] items-center">
