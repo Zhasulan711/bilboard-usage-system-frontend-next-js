ba5110be75a956064d36f2d1fe5af4806f8ee8cd
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-03-11 22:52:50 +0500

    adding fetch logic to YourBudget

--- src/components/YourBudget/YourBudget.tsx
+++ src/components/YourBudget/YourBudget.tsx
@@ -1,14 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import {
   TengeLargeCurrencyIcon,
   TengeSmallCurrencyIcon,
 } from "@/components/Icons";
 
 export default function YourBudget() {
+  const [budget, setBudget] = useState({
+    total: "1,120,448", // need to do Empty with real API
+    spentThisMonth: "534,234", // need to do Empty with real API
+    spentLastMonth: "413,323", // need to do Empty with real API
+  });
+
+  useEffect(() => {
+    fetch("https://your-api-url.com/endpoint")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setBudget({
+          total: data.total,
+          spentThisMonth: data.spentThisMonth,
+          spentLastMonth: data.spentLastMonth,
+        });
+      })
+      .catch((error) => {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+      });
+  }, []);
+
   return (
     <div className="bg-[#0F1623] ml-[20px] w-[321px] h-[160px] rounded-lg flex flex-col pl-[26px] pt-[12px]">
       <h1 className="text-white text-[28px] font-medium">Your budget</h1>
       <div className="flex flex-row items-center space-x-[3px]">
-        <h1 className="text-white text-5xl font-normal">1,120,448</h1>
+        <h1 className="text-white text-5xl font-normal">{budget.total}</h1>
         <TengeLargeCurrencyIcon />
       </div>
       <div className="flex flex-row space-x-[45px] pt-[5px]">
@@ -17,7 +49,9 @@ export default function YourBudget() {
             Spent this month
           </h3>
           <div className="flex flex-row items-center space-x-[3px]">
-            <h3 className="text-[#CFD0D3] text-xl font-normal">534,234</h3>
+            <h3 className="text-[#CFD0D3] text-xl font-normal">
+              {budget.spentThisMonth}
+            </h3>
             <TengeSmallCurrencyIcon />
           </div>
         </div>
@@ -26,7 +60,9 @@ export default function YourBudget() {
             Spent last month
           </h3>
           <div className="flex flex-row items-center space-x-[3px]">
-            <h3 className="text-[#CFD0D3] text-xl font-normal">413,323</h3>
+            <h3 className="text-[#CFD0D3] text-xl font-normal">
+              {budget.spentLastMonth}
+            </h3>
             <TengeSmallCurrencyIcon />
           </div>
         </div>
