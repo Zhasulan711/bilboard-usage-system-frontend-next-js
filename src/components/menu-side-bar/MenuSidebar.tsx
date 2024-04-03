88450c7d6413c7a8baf517fedd78b996932ddd44
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-04-03 11:36:40 +0500

    adding user button and settings page

--- src/components/menu-side-bar/MenuSidebar.tsx
+++ src/components/menu-side-bar/MenuSidebar.tsx
@@ -1,10 +1,10 @@
-import { HeaderNavBars } from "@/components/menu-side-bar/HeaderNavBars";
+import { MenuSideNavBars } from "@/components/menu-side-bar/MenuSideNavBars";
 import { PostNewAds } from "@/components/menu-side-bar/PostNewAds";
 
 export const MenuSidebar = () => {
   return (
     <aside className="w-[342px] bg-[#0F1623] h-[100vh] pt-[88px] px-[30px] flex flex-col">
-      <HeaderNavBars />
+      <MenuSideNavBars />
       <PostNewAds />
     </aside>
   );
