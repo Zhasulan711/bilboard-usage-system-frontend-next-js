94f4cd1303d47b18f05d3f0247f6f4dad2983db6
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-04-07 13:02:27 +0500

    changing all pages side bar logic

--- src/app/(routes)/transaction/page.tsx
+++ src/app/(routes)/transaction/page.tsx
@@ -27,85 +27,77 @@ export default function Home() {
   ];
 
   return (
-    <div className="relative">
-      <div className="flex flex-row absolute">
-        <MenuSidebar />
-        <Header />
+    <div className="flex flex-col">
+      <div className="flex flex-row space-x-[275px]">
+        <div className="bg-[#0F1623] rounded-lg h-[40px] w-[451px] flex flex-row items-center pl-[20px] space-x-[20px]">
+          <SearchIcon />
+          <h2 className="text-[#575C65] text-xl font-normal">Search</h2>
+        </div>
+
+        {/* navbars */}
+        <div className="flex flex-row space-x-[10px]">
+          {navItems.map(({ name, width }, index) => (
+            <div
+              className="h-[40px] flex items-center justify-center bg-[#0B101F] text-xl font-normal text-white rounded-lg"
+              style={{ width: width }}
+              key={index}
+            >
+              {name}
+            </div>
+          ))}
+        </div>
       </div>
 
-      {/* searchBar */}
-      <main className="absolute inset-0 left-[362px] top-[88px]">
-        <div className="flex flex-row space-x-[275px]">
-          <div className="bg-[#0F1623] rounded-lg h-[40px] w-[451px] flex flex-row items-center pl-[20px] space-x-[20px]">
-            <SearchIcon />
-            <h2 className="text-[#575C65] text-xl font-normal">Search</h2>
-          </div>
-
-          {/* navbars */}
-          <div className="flex flex-row space-x-[10px]">
-            {navItems.map(({ name, width }, index) => (
-              <div
-                className="h-[40px] flex items-center justify-center bg-[#0B101F] text-xl font-normal text-white rounded-lg"
-                style={{ width: width }}
-                key={index}
-              >
-                {name}
-              </div>
-            ))}
-          </div>
-        </div>
-
-        {/* table */}
-        <div className="overflow-y-auto max-h-[930px] border rounded-lg mt-[16px] border-transparent">
-          <table className="table-fixed divide-y-[32px] divide-[#010714]">
-            <thead className="text-[#B7B9BE] font-normal bg-[#0F1623]">
-              <tr className="">
-                {navTable.map((item, index) => (
-                  <th
-                    scope="col"
-                    className="px-[26px] py-[18px] text-left"
-                    key={index}
-                  >
-                    {item}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody className="text-white text-lg font-normal bg-[#0F1623] divide-y-[16px] divide-[#010714]">
-              {TRANSACTION_TABLE_LIST.map(
-                (
-                  {
-                    address,
-                    price,
-                    grp,
-                    time,
-                    placeNumber,
-                    size,
-                    category,
-                    date,
-                    status,
-                  },
-                  index
-                ) => {
-                  return (
-                    <tr key={index}>
-                      <td className="px-6 py-4">{address}</td>
-                      <td className="px-6 py-4">{price}</td>
-                      <td className="px-6 py-4">{grp}</td>
-                      <td className="px-6 py-4">{time}</td>
-                      <td className="px-6 py-4">{placeNumber}</td>
-                      <td className="px-6 py-4">{size}</td>
-                      <td className="px-6 py-4">{category}</td>
-                      <td className="px-6 py-4">{date}</td>
-                      <td className="px-6 py-4">{status}</td>
-                    </tr>
-                  );
-                }
-              )}
-            </tbody>
-          </table>
-        </div>
-      </main>
+      {/* table */}
+      <div className="overflow-y-auto max-h-[930px] border rounded-lg mt-[16px] border-transparent">
+        <table className="table-fixed divide-y-[32px] divide-[#010714]">
+          <thead className="text-[#B7B9BE] font-normal bg-[#0F1623]">
+            <tr className="">
+              {navTable.map((item, index) => (
+                <th
+                  scope="col"
+                  className="px-[26px] py-[18px] text-left"
+                  key={index}
+                >
+                  {item}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="text-white text-lg font-normal bg-[#0F1623] divide-y-[16px] divide-[#010714]">
+            {TRANSACTION_TABLE_LIST.map(
+              (
+                {
+                  address,
+                  price,
+                  grp,
+                  time,
+                  placeNumber,
+                  size,
+                  category,
+                  date,
+                  status,
+                },
+                index
+              ) => {
+                return (
+                  <tr key={index}>
+                    <td className="px-6 py-4">{address}</td>
+                    <td className="px-6 py-4">{price}</td>
+                    <td className="px-6 py-4">{grp}</td>
+                    <td className="px-6 py-4">{time}</td>
+                    <td className="px-6 py-4">{placeNumber}</td>
+                    <td className="px-6 py-4">{size}</td>
+                    <td className="px-6 py-4">{category}</td>
+                    <td className="px-6 py-4">{date}</td>
+                    <td className="px-6 py-4">{status}</td>
+                  </tr>
+                );
+              }
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
