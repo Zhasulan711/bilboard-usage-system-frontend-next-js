790945a9ec1aff43c8b7e75cfc61fc53cb346959
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-04-18 11:20:45 +0500

    landing page 01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { LoginButton } from "@/components/auth/login-button";
+import { RegisterButton } from "@/components/auth/register-button";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -11,22 +12,27 @@ const font = Poppins({
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center ">
-      <div className="space-y-6 text-center">
+      <div className="space-y-6 text-center flex flex-col items-center">
         <h1
           className={cn(
             "text-6xl font-semibold text-white drop-shadow-md",
             font.className
           )}
         >
-          🔐 Auth
+          HAPPY CLIENT
         </h1>
-        <p className="text-white text-lg">A simple authentication service</p>
-        <div>
+       
+        <div className="flex flex-row space-x-[16px]">
           <LoginButton>
-            <Button variant="secondary" size="lg">
-              Sign in
+            <Button variant="secondary" size="lg" className="w-[83px] h-[38px] px-[18px] py-[7px] rounded-[100px] border border-amber-500 justify-center items-center hover:bg-amber-500 text-white hover:text-black bg-black">
+              Login
             </Button>
           </LoginButton>
+          <RegisterButton>
+            <Button variant="secondary" size="lg" className="w-[83px] h-[38px] px-[18px] py-[7px] rounded-[100px] border border-amber-500 justify-center items-center hover:bg-amber-500 text-white hover:text-black bg-black">
+              Register
+            </Button>
+          </RegisterButton>
         </div>
       </div>
     </main>
---
+++ src/components/auth/register-button.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+}
+
+export const RegisterButton = ({
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginButtonProps) => {
+  const router = useRouter();
+  const onClick = () => {
+    router.push("/auth/register");
+  };
+
+  if (mode === "modal") {
+    return <span>TODO: implement modal</span>;
+  }
+
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
+};
