b1a1319617e5416fa55b28014b675a32a37c1b49
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-05-26 21:13:03 +0500

    landing page update

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Swipe from "react-easy-swipe";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import Link from "next/link";
 import { IMAGES } from "@/constants/LandingPageImages";
+import { useRouter } from "next/navigation";
 
 export default function LandingPage() {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const router = useRouter();
 
   const handleNextSlide = () => {
     let newSlide = currentSlide === IMAGES.length - 1 ? 0 : currentSlide + 1;
@@ -19,11 +21,22 @@ export default function LandingPage() {
     setCurrentSlide(newSlide);
   };
 
+  const handleClickImageToRouting = () => {
+    router.push("/post-add");
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleNextSlide();
+    }, 3000); // Change image every 3 seconds
+    return () => clearInterval(interval);
+  }, [currentSlide]);
+
   return (
     <div className="relative h-screen">
       <Link
         href="/post-add"
-        className="absolute z-20 w-[100px] h-[40px] bg-amber-500 flex justify-center items-center bottom-[70px] right-[60px]
+        className="absolute z-20 w-[120px] h-[40px] bg-amber-500 flex justify-center items-center bottom-[70px] right-[60px]
       text-black text-[20px] font-normal uppercase hover:bg-orange-500 cursor-pointer"
       >
         buy ad
@@ -31,7 +44,7 @@ export default function LandingPage() {
       <AiOutlineLeft
         onClick={handlePrevSlide}
         className="absolute z-20 inset-y-1/2 left-0 cursor-pointer text-white ml-[60px]
-        w-[62px] h-[62px] bg-black opacity-30 rounded-[76px]"
+        w-[62px] h-[62px] bg-black opacity-30 rounded-[76px] hover:opacity-80"
       />
       <div className="h-screen flex overflow-hidden relative">
         <Swipe
@@ -45,7 +58,8 @@ export default function LandingPage() {
                 <img
                   key={index}
                   src={image.imageURL}
-                  className="imageCover absolute z-10"
+                  className="imageCover absolute z-10 cursor-pointer"
+                  onClick={handleClickImageToRouting}
                 />
               );
             }
@@ -55,7 +69,7 @@ export default function LandingPage() {
       <AiOutlineRight
         onClick={handleNextSlide}
         className="absolute z-20 right-0 inset-y-1/2 cursor-pointer text-white mr-[60px]
-        w-[62px] h-[62px] bg-black opacity-30 rounded-[76px]"
+        w-[62px] h-[62px] bg-black opacity-30 rounded-[76px] hover:opacity-80"
       />
     </div>
   );
